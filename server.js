dd17997d8db1fb425a7161f5416508fb49e6f319
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-05-23 13:23:29 +0200

    api endpoint added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import { join, dirname } from "path"
 import { fileURLToPath } from "url"
 import { readFile, writeFile } from "node:fs/promises"
+import { dispatchCalls } from "./backend/dispatcher.js"
 
 const app = express()
 const PORT = 3000
@@ -13,6 +14,16 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
     res.sendFile(join(staticDir, "/index.html"))
+})
+
+app.post("/api", async (req, res) => {
+    try {
+        const calls = req.body
+        const result = await dispatchCalls(calls)
+        res.send(result)
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 app.listen(PORT, function () {
